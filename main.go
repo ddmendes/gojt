e95078bbd167de1f0109eb2b81398bcb524461d8
Davi Mendes <ddioriomendes@gmail.com>
2020-05-23 14:23:54 -0300

    Beautify piped JSON document

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -13,27 +14,50 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(pipedData)
+
+	jsonDoc, err := unmarshalJSON(pipedData)
+	if err != nil {
+		panic(err)
+	}
+
+	output, err := marshalJSON(jsonDoc, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(output))
 }
 
-func getPipedData() (string, error) {
+func getPipedData() ([]byte, error) {
 	info, err := os.Stdin.Stat()
 	if err != nil {
-		return "", err
+		return []byte{}, err
 	}
 
 	if info.Mode()&os.ModeNamedPipe != 0 && info.Size() > 0 {
-		var output []rune
+		var output []byte
 		reader := bufio.NewReader(os.Stdin)
 		for {
-			input, _, err := reader.ReadRune()
+			input, err := reader.ReadByte()
 			if err == io.EOF {
 				break
 			}
 			output = append(output, input)
 		}
-		return string(output), nil
-	} else {
-		return "", errors.New("No piped data")
+		return []byte(output), nil
 	}
+	return []byte{}, errors.New("No piped data")
+}
+
+func unmarshalJSON(input []byte) (interface{}, error) {
+	var document interface{}
+	err := json.Unmarshal(input, &document)
+	return document, err
+}
+
+func marshalJSON(document interface{}, beautify bool) ([]byte, error) {
+	if beautify {
+		return json.MarshalIndent(document, "", "    ")
+	}
+	return json.Marshal(document)
 }
